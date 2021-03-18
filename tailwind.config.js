a1c0158a3686470843e7cb03dd1e49ae202d4ac8
neelam618 <you@example.com>
2021-03-18 18:11:56 +0530

    added custom color and used it for btn and h1 color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-blue': '#1992d4',
+      },
+    }
   },
   variants: {
     extend: {
